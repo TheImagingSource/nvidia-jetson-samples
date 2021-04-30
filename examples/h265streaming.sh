437455e44830e84210848040913821d62c3ae886
marat.faizov@theimagingsource.com <marat.faizov@theimagingsource.com>
2021-04-30 16:24:06 +0200

    Fixed small typo in h265 shell script

--- examples/h265streaming.sh
+++ examples/h265streaming.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-xfce4-terminal -e 'bash -c "echo h264 streaming can be stopped with Ctrl+C;echo Start streaming by pressing ENTER;read;gst-launch-1.0 -v nvarguscamerasrc ! nvv4l2h265enc ! h265parse config-interval=-1 ! rtph265pay ! udpsink host=239.0.0.1 port=5000; bash"'
+xfce4-terminal -e 'bash -c "echo h265 streaming can be stopped with Ctrl+C;echo Start streaming by pressing ENTER;read;gst-launch-1.0 -v nvarguscamerasrc ! nvv4l2h265enc ! h265parse config-interval=-1 ! rtph265pay ! udpsink host=239.0.0.1 port=5000; bash"'
